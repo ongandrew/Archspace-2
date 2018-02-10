9bef07e6f978d4c9af4097a452a73290249cebb0
Andrew Ong <ong.andrew@gmail.com>
2018-02-11 01:27:15 +1100

    Planet invest pool.

--- Archspace2.Web/Controllers/PlayerController.cs
+++ Archspace2.Web/Controllers/PlayerController.cs
@@ -148,5 +148,86 @@ namespace Archspace2.Web
                 return BadRequest();
             }
         }
+
+        [HttpPost]
+        [Route("change_investment_pool_usage")]
+        public async Task<IActionResult> ChangeInvestmentPoolUsage([FromForm]int[] ids)
+        {
+            try
+            {
+                using (DatabaseContext context = Game.GetContext())
+                {
+                    context.Attach(Game.Universe);
+
+                    User user = await context.GetUserAsync(User);
+                    Player player = Game.Universe.Players.Where(x => x.User != null && x.User.Id == user.Id).Single();
+
+                    foreach (Planet planet in player.Planets)
+                    {
+                        planet.UsePlanetInvestmentPool = false;
+                    }
+
+                    foreach (int id in ids)
+                    {
+                        Planet planet = player.Planets.SingleOrDefault(x => x.Id == id);
+                        if (planet != null)
+                        {
+                            planet.UsePlanetInvestmentPool = true;
+                        }
+                    }
+
+                    await context.SaveChangesAsync();
+
+                    return RedirectToAction("PlanetInvestPool", "Archspace");
+                }
+            }
+            catch (Exception e)
+            {
+                await Game.LogAsync(e);
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("change_investment_pool")]
+        public async Task<IActionResult> ChangeInvestmentPool([FromForm]int amount)
+        {
+            try
+            {
+                using (DatabaseContext context = Game.GetContext())
+                {
+                    context.Attach(Game.Universe);
+
+                    User user = await context.GetUserAsync(User);
+                    Player player = Game.Universe.Players.Where(x => x.User != null && x.User.Id == user.Id).Single();
+
+                    if (amount <= player.Resource.ProductionPoint && amount > 0)
+                    {
+                        int trueAmount = amount;
+
+                        if (int.MaxValue - player.PlanetInvestmentPool < amount)
+                        {
+                            trueAmount = int.MaxValue - player.PlanetInvestmentPool;
+                        }
+                        else
+                        {
+                            trueAmount = amount;
+                        }
+
+                        player.PlanetInvestmentPool += trueAmount;
+                        player.Resource.ProductionPoint -= trueAmount;
+
+                        await context.SaveChangesAsync();
+                    }
+
+                    return RedirectToAction("PlanetInvestPool", "Archspace");
+                }
+            }
+            catch (Exception e)
+            {
+                await Game.LogAsync(e);
+                return BadRequest();
+            }
+        }
     }
 }
